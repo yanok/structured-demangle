21f5b5b5c12e38e4406dcbd38afedc7248981530
Ilya Yanok <ilya.yanok@weka.io>
2024-12-17 11:47:54 +0100

    fix how it is called

--- source/structured_demangle.d
+++ source/structured_demangle.d
@@ -11,22 +11,21 @@ Node structuredDemangle(return scope const(char)[] buf, return scope char[] dst 
         auto res = demangleCXX(buf, __cxa_demangle, dst);
         return Node(Node.Kind.CxxMangled, cast(string) res);
     }
-    SymbolBuilder b;
-    b.start;
-    auto d = Demangle!()(buf, dst, b);
+    auto d = Demangle!(SymbolBuilder)(buf, dst);
+    d.hooks.start;
     // fast path (avoiding throwing & catching exception) for obvious
     // non-D mangled names
     if (buf.length < 2 || !(buf[0] == 'D' || buf[0 .. 2] == "_D"))
     {
-        auto res = d.dst.copyInput(buf);
+        auto res = d.dst.copyInput(buf).idup;
         return Node(Node.Kind.NonMangled, res);
     }
-    auto demangled = d.demangleName();
+    auto demangled = d.demangleName().idup;
     if (demangled == buf)
     {
         return Node(Node.Kind.NonMangled, demangled);
     }
-    auto res = b.end;
+    auto res = d.hooks.end;
     assert(res.value == demangled);
     return res;
 }
